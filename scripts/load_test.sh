5b10e3239d83cb80bd8bef65935db1bc57e76536
Rafay-devsinc <m.rafay.haq@devsinc.com>
2025-09-10 20:56:04 +0500

    Update load_test.sh

--- scripts/load_test.sh
+++ scripts/load_test.sh
@@ -6,8 +6,12 @@ RESULTS="load_test_results.txt"
 
 echo "Running load test on foo.localhost and bar.localhost..."
 
-hey -z $DURATION -q 10 -c 5 http://foo.localhost/ > foo_results.txt
-hey -z $DURATION -q 10 -c 5 http://bar.localhost/ > bar_results.txt
+# hey -z $DURATION -q 10 -c 5 http://foo.localhost/ > foo_results.txt
+# hey -z $DURATION -q 10 -c 5 http://bar.localhost/ > bar_results.txt
+
+
+hey -z 10s -q 10 -c 5 http://localhost:8081/ > load_test_results.txt
+hey -z 10s -q 10 -c 5 http://localhost:8082/ >> load_test_results.txt
 
 echo "==== FOO ====" > $RESULTS
 cat foo_results.txt >> $RESULTS
